96f67c3513e65809728f98849b9a77ca489dad91
Mike Farrow <contact@mikefarrow.co.uk>
2015-12-26 04:11:39 +0000

    accounting for port in proxy pass variable

--- config/start.sh
+++ config/start.sh
@@ -5,7 +5,7 @@ sed -i "s/%proxy_pass%/$PROXY_PASS/" /etc/nginx/nginx.conf
 
 echo "$AUTH_USERS" > /etc/nginx/passwords
 
-until ping -c 1 $PROXY_PASS
+until ping -c 1 ${PROXY_PASS%:*}
 do
   echo "trying to connect with upstream"
   sleep 1
