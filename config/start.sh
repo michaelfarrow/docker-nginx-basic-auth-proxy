cb9decff132d6981988b17b75b66207e3faff203
Mike Farrow <contact@mikefarrow.co.uk>
2015-12-26 04:00:43 +0000

    pinging upstream before starting

--- config/start.sh
+++ config/start.sh
@@ -5,4 +5,10 @@ sed -i "s/%proxy_pass%/$PROXY_PASS/" /etc/nginx/nginx.conf
 
 echo "$AUTH_USERS" > /etc/nginx/passwords
 
+until ping -c 1 $PROXY_PASS
+do
+  echo "trying to connect with upstream"
+  sleep 1
+done
+
 nginx -g 'daemon off;'
